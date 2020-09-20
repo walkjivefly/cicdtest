af57323c7769c11fcdf07283d7c560fe81137933
Mark Brooker <mark@walkjivefly.com>
2020-09-20 14:29:01 +0200

    Use ubuntu16, python build action and version 3.6

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ mkdir -p venv
 python3 -m virtualenv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-pyinstaller CrownSweeper.spec 
+pyinstaller --onefile --windowed CrownSweeper.spec 
